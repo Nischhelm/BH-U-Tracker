7a863cb9a323c2ac51b313aec623c09ee7331278
Nischhelm <nischhelm@gmail.com>
2025-07-10 07:53:20 +0200

    nevermind i guess its already 1.0.5?
    cf has a 1.0.4, most recent was 1.0.3 in this repo though...

--- src/main/java/bhutracker/BrokenHeartUndershirtTracker.java
+++ src/main/java/bhutracker/BrokenHeartUndershirtTracker.java
@@ -17,7 +17,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 )
 public class BrokenHeartUndershirtTracker {
     public static final String MODID = "bhutracker";
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.0.5";
     public static final String NAME = "Broken Heart & Undershirt Stat";
 
     public static StatBase UNDERSHIRT_ACTIVATED = (new StatBasic("stat.undershirt_activated", new TextComponentTranslation(TextFormatting.AQUA + "Undershirt Activated"))).registerStat();
